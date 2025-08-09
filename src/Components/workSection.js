1e79a2eb87ecae42272325a7cb9549dac9dbccce
pankaj <pankajjoshi4467@gmail.com>
2025-08-09 10:32:40 +0530

    projects are done

--- src/Components/workSection.js
+++ src/Components/workSection.js
@@ -10,23 +10,57 @@ function WorkSection(){
             <Subheading content="My Work"/>
             <div>
                 <Grid container spacing={{ xs: 2, md: 3 }}>
+                   
+                    {/* Add more Grid items here for more projects */}
+
+
                     <Grid className="work-grid-item" item xs={12} sm={6} md={4}>
                         <WorkCard 
-                            projectName={"University Website"} 
-                            projectCategory={"Web Development"} 
+                            projectName={"Edemy - Online Learning Platform"} 
+                            projectCategory={"Mern Stack"} 
+                            imgURL={require("../res/work/edemy.png")}
+                            projectURL={"https://github.com/pankaj-joshi4467/Edemy---A-Learning-Management-System.git"}
+                        />
+                    </Grid>
+
+
+                    <Grid className="work-grid-item" item xs={12} sm={6} md={4}>
+                        <WorkCard 
+                            projectName={"Imagify - AI Image Generator"} 
+                            projectCategory={"Mern Stack"} 
+                            imgURL={require("../res/work/imagify.png")}
+                            projectURL={"https://github.com/pankaj-joshi4467/Imagify.git"}
+                        />
+                    </Grid>
+
+
+                    <Grid className="work-grid-item" item xs={12} sm={6} md={4}>
+                        <WorkCard 
+                            projectName={"Crave Cart - Food Delivery App"} 
+                            projectCategory={"Mern Stack and tailwind css"} 
+                            imgURL={require("../res/work/food.png")}
+                            projectURL={"https://github.com/pankaj-joshi4467/Crave-Cart.git"}
+                        />
+                    </Grid>
+
+
+
+                     <Grid className="work-grid-item" item xs={12} sm={6} md={4}>
+                        <WorkCard 
+                            projectName={"University Portal"} 
+                            projectCategory={"ReactJs"} 
                             imgURL={require("../res/work/uni.png")}
                             projectURL={"https://relaxed-dolphin-7a34fa.netlify.app/"}
                         />
                     </Grid>
                     <Grid className="work-grid-item" item xs={12} sm={6} md={4}>
                         <WorkCard 
-                            projectName={"Tour and Travel Website"} 
+                            projectName={"Tour and Travel Booking"} 
                             projectCategory={"ReactJs"} 
                             imgURL={require("../res/work/tour.png")}
                             projectURL={"https://idyllic-lamington-e962b3.netlify.app/"}
                         />
                     </Grid>
-                    {/* Add more Grid items here for more projects */}
                 </Grid>
             </div>
         </div>
